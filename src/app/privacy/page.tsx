0bda647dfaaa57428fd3cc043181ea322f5c0d02
Elton Giglio Kasesky <eltonkasesky9@gmail.com>
2024-09-09 14:12:01 -0300

    Update privacy;

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/header/header";
 import Privacy from "@/components/privacy/Privacy";
 import Footer from "@/components/footer/footer";
 import Copyright from "@/components/footer/copyright";
+import Link from "next/link";
 
 export default function PrivacyPage(){
     return (
